140cb849681991a6dce66e7448c287a1966459b8
qinqingshan <qinqingshan@elibot.cn>
2023-05-31 14:16:42 +0800

    增加servol测试数据。

--- rtsi/test.cpp
+++ rtsi/test.cpp
@@ -4,7 +4,11 @@
 #include "rtsi_control_interface.h"
 #include "script_client.h"
 
+#include <iostream>
+#include <fstream>
+#include <stdlib.h>
 
+using namespace std;
 
 int main()
 {
@@ -12,11 +16,102 @@ int main()
 
 	//cs_rtsi::RTSIReceiveInterface recv("192.168.40.27");
 	//cs_rtsi::RTSIControlInterface control("192.168.40.27");
-	cs_rtsi::RTSIControlInterface control("192.168.40.27");
+	std::string host = "172.21.113.154";
+	std::string dash_host = "127.0.0.1";
+	cs_rtsi::RTSIControlInterface control(host);
 	std::vector<double> speed = { 0, 0, 0,0.05, 0, 0 };
 	std::vector<double> speed_vector = { 0.0, 0.0, 0.1, 0, 0.0, 0.0 };
-	control.speedJ({ 0.5,0.1,0.1,0.1,0.1,0.1 }, 0.1);
-	cs_rtsi::DashboardClient dash("192.168.40.27");
+	//control.speedJ({ 0.5,0.1,0.1,0.1,0.1,0.1 }, 0.1);
+
+	//ofstream outfile;
+	//outfile.open(R"(D:\Work\CS\Code\RTSI\cs_rtsi\test\test_data\tcp_path_2.txt)", ios::out | ios::app);  // 需要目录下有该文件
+	//if (!outfile.is_open())
+	//{
+	//	cout << "read outfile failed" << endl;
+	//	return -1;
+	//}
+
+	while (true) {
+
+		ifstream readFile;
+		readFile.open(R"(D:\Work\CS\Code\RTSI\cs_rtsi\test\test_data\tcp_path_2.txt)");
+		//readFile.open(R"(D:\Work\CS\Code\RTSI\cs_rtsi\test\test_data\joint_path.txt)");
+	
+		vector<string> file_in;
+		if (readFile) {
+			cout << "文件打开成功！" << endl;
+			string buf;
+			char sep = ' ';
+			while (getline(readFile, buf)) {
+				int index1 = buf.find_first_of(sep);     //查找逗号分隔符        
+				string str1 = buf.substr(0, index1);
+				string leftstring = buf.substr(index1 + 1, buf.length());
+
+				int index2 = leftstring.find_first_of(sep);
+				string str2 = leftstring.substr(0, index2);
+				leftstring = leftstring.substr(index2 + 1, leftstring.length());
+
+				int index3 = leftstring.find_first_of(sep);
+				string str3 = leftstring.substr(0, index3);
+				leftstring = leftstring.substr(index3 + 1, leftstring.length());
+
+				int index4 = leftstring.find_first_of(sep);
+				string str4 = leftstring.substr(0, index4);
+				leftstring = leftstring.substr(index4 + 1, leftstring.length());
+
+				int index5 = leftstring.find_first_of(sep);
+				string str5 = leftstring.substr(0, index5);
+				leftstring = leftstring.substr(index5 + 1, leftstring.length());
+
+				int index6 = leftstring.find_first_of(sep);
+				string str6 = leftstring.substr(0, index6);
+				leftstring = leftstring.substr(index6 + 1, leftstring.length());
+
+
+				//float x = std::stof(str1);  // 字符串转浮点型
+				//float y = std::stof(str2);
+				//float z = std::stof(str3);
+				vector<double> data;
+				double j0 = std::stod(str1);// 字符串转双精度
+				double j1 = std::stod(str2);
+				double j2 = std::stod(str3);
+				double j3 = std::stod(str4);// 字符串转双精度
+				double j4 = std::stod(str5);
+				double j5 = std::stod(str6);
+				data.push_back(j0);
+				data.push_back(j1);
+				data.push_back(j2);
+				data.push_back(j3);
+				data.push_back(j4);
+				data.push_back(j5);
+				//vector<double> joints = data;
+				//vector<double> joints = control.getInverseKinematics(data);
+				//std:vector<double> tcp = control.getForwardKinematics(joints);
+				//joints = control->getInverseKinematics(joints);
+				//point3 p{ xx,yy,zz };
+				//vec3.push_back(p);
+				//outfile << tcp[0] << sep << tcp[1] << sep << tcp[2] << sep << tcp[3] << sep << tcp[4] << sep << tcp[5] << endl;  //输出到文件
+				control.servoL(data, 0, 0, 0.08, 0.2, 500);
+				Sleep(70);
+				//time_t timep;
+				//time(&timep);
+				//char tmp[64];
+				//strftime(tmp, sizeof(tmp), "%Y-%m-%d %H:%M:%S", localtime(&timep));
+				//cout << "currentTime is " << tmp << "." << endl;
+				// file_in.push_back(temp);
+			}
+		}
+		else {
+			cerr << "文件打开失败!" << endl;
+		}
+		readFile.close();
+		//打印读取的输出
+		for (const auto& i : file_in)
+			cout << i << endl;
+	}
+
+
+	cs_rtsi::DashboardClient dash(dash_host);
 	while (true)
 	{
 		if (!dash.isConnected())
@@ -24,7 +119,6 @@ int main()
 			if (dash.connect())
 			{
 				std::string str = dash.robotMode();
-
 				std::cout << str << std::endl;
 			}
 			else
@@ -39,6 +133,9 @@ int main()
 			std::string str = dash.robotMode();
 
 			std::cout << str << std::endl;
+			if (str.find("RUNNING")!=-1 ) {
+				break;
+			}
 
 		}
 
@@ -46,7 +143,7 @@ int main()
 	}
 
 
-	cs_rtsi::RTSIReceiveInterface recv("192.168.40.27");
+	cs_rtsi::RTSIReceiveInterface recv(host);
 
 
 
